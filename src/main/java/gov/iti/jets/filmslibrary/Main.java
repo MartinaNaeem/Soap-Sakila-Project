47aa47cfb462f8de8a714fa8ac04da785735ae66
MartinaNaeem <martina9naem@gmail.com>
2023-04-19 23:19:19 +0200

    main

--- src/main/java/gov/iti/jets/filmslibrary/Main.java
+++ src/main/java/gov/iti/jets/filmslibrary/Main.java
@@ -1,10 +1,8 @@
 package gov.iti.jets.filmslibrary;
 
-import gov.iti.jets.filmslibrary.services.ActorFilmsService;
 
 public class Main {
     public static void main(String args[]) {
-            System.out.println(new ActorFilmsService().getFilmsOFAnActor((short) 1).getFilms().get(0));
 
     }
 }
