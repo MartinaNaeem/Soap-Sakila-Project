18a27d6d625d1be4c1a79bb0d4c40983363a8d83
MartinaNaeem <martina9naem@gmail.com>
2023-04-19 23:01:17 +0200

    customer service

--- src/main/java/gov/iti/jets/filmslibrary/mappers/CustomerMapper.java
+++ src/main/java/gov/iti/jets/filmslibrary/mappers/CustomerMapper.java
@@ -1,32 +1,50 @@
 package gov.iti.jets.filmslibrary.mappers;
 
-import gov.iti.jets.filmslibrary.dtos.CustomerGetterDto;
-import gov.iti.jets.filmslibrary.dtos.CustomerSetterDto;
+import gov.iti.jets.filmslibrary.dtos.addressDtos.AddressSetterDto;
+import gov.iti.jets.filmslibrary.dtos.customerDtos.CustomerGetterDto;
+import gov.iti.jets.filmslibrary.dtos.rentsDtos.CustomerRentsGetterDto;
+import gov.iti.jets.filmslibrary.dtos.customerDtos.CustomerSetterDto;
 import gov.iti.jets.filmslibrary.model.*;
+import gov.iti.jets.filmslibrary.util.Rents;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CustomerMapper {
 
-    public CustomerGetterDto toCustomerGetterDto(CustomerList customer) {
-        CustomerGetterDto customerGetterDto = new CustomerGetterDto();
-        customerGetterDto.setName(customer.getName());
-        customerGetterDto.setId(customer.getId());
-        customerGetterDto.setAddress(customer.getAddress());
-        customerGetterDto.setCity(customer.getCity());
-        customerGetterDto.setCountry(customer.getCountry());
-        customerGetterDto.setNotes(customer.getNotes());
-        customerGetterDto.setZipcode(customer.getZipCode());
-        customerGetterDto.setPhone(customer.getPhone());
-        customerGetterDto.setAddress(customer.getAddress());
-        customerGetterDto.setEmail(customer.getEmail());
+    public CustomerGetterDto toCustomerGetterDto(Customer customer) {
+        CustomerGetterDto customerGetterDto = new CustomerGetterDto()
+                .builder()
+                .id(customer.getCustomerId())
+                .addressId(customer.getAddressId().getAddressId())
+                .name(customer.getFirstName()+" "+customer.getLastName())
+                .address(customer.getAddressId().getAddress())
+                .city(customer.getAddressId().getCityId().getCity())
+                .postalCode(customer.getAddressId().getPostalCode())
+                .phone(customer.getAddressId().getPhone())
+                .email(customer.getEmail())
+                .createDate(customer.getCreateDate())
+                .country(customer.getAddressId().getCityId().getCountryId().getCountry())
+                .build();
+
+        if(customer.getRentalList().size()!=0){
+            List<CustomerRentsGetterDto> customerRentsGetterDtoList = new ArrayList<>();
+            for(Rental rental: customer.getRentalList()){
+                customerRentsGetterDtoList.add(Rents.getCustomerRents(rental));
+            }
+
+            customerGetterDto.setCustomerRentsGetterDtoList(customerRentsGetterDtoList);
+        }
+        if(customer.getAddressId().getLocation()!= null)
+            customerGetterDto.setLocation(customer.getAddressId().getLocation());
+
         return customerGetterDto;
     }
 
     public Customer toCustomerEntity(CustomerSetterDto customerSetterDto) {
         Customer customer = new Customer();
-        customer.setActive(customerSetterDto.isActive());
+        customer.setActive(customerSetterDto.getActive());
         customer.setCreateDate(new Date());
         customer.setLastUpdate(new Date());
         customer.setEmail(customerSetterDto.getEmail());
@@ -34,26 +52,26 @@ public class CustomerMapper {
         customer.setLastName(customerSetterDto.getLastName());
         customer.setPaymentList(new ArrayList<>());
         customer.setRentalList(new ArrayList<>());
-        if(customerSetterDto.getId() != 0)
+        customer.setStoreId(new Store(customerSetterDto.getStoreId()));
+        if(customerSetterDto.getId() != null)
             customer.setCustomerId(customerSetterDto.getId());
         return customer;
     }
 
-    public Address createAddress(CustomerSetterDto customerSetterDto) {
+    public AddressSetterDto toAddressSetter(CustomerSetterDto customerSetterDto) {
 
-        Address address = new Address();
-        address.setAddress(customerSetterDto.getAddress());
-        address.setAddress2(customerSetterDto.getAddress2());
-        address.setPhone(customerSetterDto.getPhone());
-        address.setPostalCode(customerSetterDto.getPostalCode());
-        address.setDistrict(customerSetterDto.getDistrict());
-        address.setPhone(customerSetterDto.getPhone());
-//        if(customerSetterDto.getAddressID() !=0){
-//            System.out.println("idddd: " + customerSetterDto.getAddressID());
-//            address.setAddressId(customerSetterDto.getAddressID());
-//        }
-        address.setLastUpdate(new Date());
-//        address.setLocation(entityManager.find(Address.class, 1).getLocation());
+        AddressSetterDto address = new AddressSetterDto();
+        address.setAddress(customerSetterDto.getAddress().getAddress());
+        address.setAddress2(customerSetterDto.getAddress().getAddress2());
+        address.setPhone(customerSetterDto.getAddress().getPhone());
+        address.setPostalCode(customerSetterDto.getAddress().getPostalCode());
+        address.setDistrict(customerSetterDto.getAddress().getDistrict());
+        address.setPhone(customerSetterDto.getAddress().getPhone());
+        address.setCountry(customerSetterDto.getAddress().getCountry());
+        address.setCity(customerSetterDto.getAddress().getCity());
+        if(customerSetterDto.getAddress().getAddressId()!=null){
+            address.setAddressId(customerSetterDto.getAddress().getAddressId());
+        }
         return address;
 
     }
