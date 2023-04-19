e92a5ba0e734c804e2559b29b7e81cc1c1bfb0c4
MartinaNaeem <martina9naem@gmail.com>
2023-04-19 23:01:43 +0200

    entity factory

--- src/main/java/gov/iti/jets/filmslibrary/repository/EntityFactory.java
+++ src/main/java/gov/iti/jets/filmslibrary/repository/EntityFactory.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class EntityFactory {
-    public static final EntityManagerFactory emf =
+    public volatile static EntityManagerFactory emf =
             Persistence.createEntityManagerFactory("sakila");
 
 }
